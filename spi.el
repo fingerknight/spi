afdea5d407cfabf1e66574db5c4567759028a6a1
fingerknight <noselord@tutanota.com>
2023-06-26 00:40:51 +0800

    replace '\t' with 4 spaces

--- spi.el
+++ spi.el
@@ -61,25 +61,25 @@ PKG should be a  symbol, and URL should be a string.
 
 If PKG has already existed, then try to reinstall it."
   (let* ((pkg-name (symbol-name pkg))
-		 (dir (expand-file-name pkg-name spi-repo-directory)))
-	(when (spi-installed-p pkg)
-	  (message "Package %s exists. Trying to reinstall." pkg-name)
-	  (spi-remove pkg))
-	(message "Fetching %s..." pkg-name)
-	(with-temp-buffer
-	  (if (= (call-process "git" nil t nil
-							   "clone" url dir)
-				 0)
-		  (spi-build pkg)
-		(message (buffer-string))))))
+         (dir (expand-file-name pkg-name spi-repo-directory)))
+    (when (spi-installed-p pkg)
+      (message "Package %s exists. Trying to reinstall." pkg-name)
+      (spi-remove pkg))
+    (message "Fetching %s..." pkg-name)
+    (with-temp-buffer
+      (if (= (call-process "git" nil t
+                           "clone" url dir)
+             0)
+          (spi-build pkg)
+        (message (buffer-string))))))
 
 (defun spi--byte-compile-dest-file (filename)
   "Return the destination of compiled file."
   (expand-file-name 
    (file-name-with-extension
-	(file-relative-name filename
-						spi-repo-directory)
-	"elc")
+    (file-relative-name filename
+                        spi-repo-directory)
+    "elc")
    spi-build-directory))
 
 (defun spi-build (pkg)
@@ -87,53 +87,53 @@ If PKG has already existed, then try to reinstall it."
 except file's name starts with dot `.'.
 PKG should be a symbol."
   (let* ((pkg-name (symbol-name pkg))
-		 (src (expand-file-name pkg-name spi-repo-directory))
-		 (dst (expand-file-name pkg-name spi-build-directory)))
-	(when (file-directory-p dst)
-	  (delete-directory dst t))
-	(if (not (spi-installed-p pkg))
-		(message "No such package: %s" pkg-name)
-	  (let ((byte-compile-dest-file-function #'spi--byte-compile-dest-file))
-		(dolist (file (directory-files-recursively src "^[^.].*"))
-		  (when (string= (file-name-extension file)
-						 "el")
-			(make-directory (file-name-directory
-							 (spi--byte-compile-dest-file file))
-							t)
-			(byte-compile-file file)))
-		(add-to-list 'load-path dst)))))
+         (src (expand-file-name pkg-name spi-repo-directory))
+         (dst (expand-file-name pkg-name spi-build-directory)))
+    (when (file-directory-p dst)
+      (delete-directory dst t))
+    (if (not (spi-installed-p pkg))
+        (message "No such package: %s" pkg-name)
+      (let ((byte-compile-dest-file-function #'spi--byte-compile-dest-file))
+        (dolist (file (directory-files-recursively src "^[^.].*"))
+          (when (string= (file-name-extension file)
+                         "el")
+            (make-directory (file-name-directory
+                             (spi--byte-compile-dest-file file))
+                            t)
+            (byte-compile-file file)))
+        (add-to-list 'load-path dst)))))
 
 (defun spi-update (pkg)
   "Update single package.
 PKG should be a symbol"
   (let* ((pkg-name (symbol-name pkg))
-		 (dir (expand-file-name pkg-name spi-repo-directory)))
-	(if (not (spi-installed-p pkg))
-		(message "No such package: %s" pkg-name)
-	  (message "Updating: %s..." pkg-name)
-	  (with-temp-buffer
-		(let ((default-directory dir))
-		  (if (= (call-process "git" nil t nil
-							   "pull")
-				 0)
-			  (spi-build pkg)
-			(message (buffer-string))))))))
+         (dir (expand-file-name pkg-name spi-repo-directory)))
+    (if (not (spi-installed-p pkg))
+        (message "No such package: %s" pkg-name)
+      (message "Updating: %s..." pkg-name)
+      (with-temp-buffer
+        (let ((default-directory dir))
+          (if (= (call-process "git" nil t nil
+                               "pull")
+                 0)
+              (spi-build pkg)
+            (message (buffer-string))))))))
 
 (defun spi-update-all ()
   "Update all packages."
   (interactive)
   (dolist (pkg-name (directory-files spi-repo-directory nil "[^.]"))
-	(spi-update (intern pkg-name))))
+    (spi-update (intern pkg-name))))
 
 (defun spi-remove (pkg)
   "Remove package.
 PKG should be a symbol."
   (let* ((pkg-name (symbol-name pkg))
-		 (dirs (list (expand-file-name pkg-name spi-repo-directory)
-					 (expand-file-name pkg-name spi-build-directory))))
-	(message "Removing package: %s" pkg-name)
-	(dolist (it dirs)
-	  (delete-directory it t))))
+         (dirs (list (expand-file-name pkg-name spi-repo-directory)
+                     (expand-file-name pkg-name spi-build-directory))))
+    (message "Removing package: %s" pkg-name)
+    (dolist (it dirs)
+      (delete-directory it t))))
 
 
 (provide 'spi)
